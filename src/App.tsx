af2ba24959f72e96c26d1a149e0c493bfb040c8f
zhengsq23 <57934926+zhengsq23@users.noreply.github.com>
2024-04-04 17:45:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
       <ContactUs setSelectedPage={setSelectedPage}/>
-      <Footer setSelectedPage={setSelectedPage}/>
+      <Footer />
     </div>
   )
 }
